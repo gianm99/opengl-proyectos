c11e6f2bc0d62df69cb64712532d417f9c94e6df
Gian Lucas Martin Chamorro <gianlucasmartinchamorro@gmail.com>
2020-06-03 22:12:43 +0200

    Añadir archivos de assimp

--- Practica6/Practica6.h
+++ Practica6/Practica6.h
@@ -13,6 +13,10 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 #include <cmath>
 #include <cstdlib>
 #include <vector>
